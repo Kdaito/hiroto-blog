03211330b23d7aee280b93373d27513ef66cba01
Kdaito <kdaito0620@gmail.com>
2024-08-11 14:31:13 +0900

    fix: ページを数えるロジックを修正

--- src/ui/pages/list.tsx
+++ src/ui/pages/list.tsx
@@ -4,7 +4,7 @@ const MAX_ITEMS = 8;
 
 export default function ListPage({ params }: { params: { id: string } }) {
   const id = parseInt(params.id, 10);
-  const start = id + (id - 1) * MAX_ITEMS;
+  const start = 1 + (id - 1) * MAX_ITEMS;
   const end = id * MAX_ITEMS;
 
   return (
