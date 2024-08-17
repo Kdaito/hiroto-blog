65fc9c43405b4f404ead184e392333d217cf3101
Kdaito <kdaito0620@gmail.com>
2024-08-17 18:03:52 +0900

    fix: リストコンポーネント単純化

--- src/ui/components/list.tsx
+++ src/ui/components/list.tsx
@@ -1,5 +1,4 @@
-import Card from "../components/card";
-import SideCategory from "./side-category";
+import Card from "@/ui/components/card";
 
 type Props = {
   pageTitle: string;
@@ -8,25 +7,20 @@ type Props = {
 
 const List: React.FC<Props> = ({ pageTitle, resultCounts }) => {
   return (
-    <div className="flex gap-5 relative">
-      <div>
-        <div className="border-b pb-4 flex justify-between items-end">
-          <h2 className="text-4xl font-bold">{pageTitle}</h2>
-          <p className="text-lg opacity-50">{resultCounts}</p>
-        </div>
-        <div className="pt-4 grid grid-cols-1 gap-2">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+    <div>
+      <div className="border-b pb-4 flex justify-between items-end">
+        <h2 className="text-4xl font-bold">{pageTitle}</h2>
+        <p className="text-lg opacity-50">{resultCounts}</p>
       </div>
-      <div className="sticky top-0 w-[280px] h-[200px] pt-16">
-        <SideCategory />
+      <div className="pt-4 grid grid-cols-1 gap-2">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
