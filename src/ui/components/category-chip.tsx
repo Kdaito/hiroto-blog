2147c924a32a8745cf727c4f2efc0368123294b1
Kdaito <kdaito0620@gmail.com>
2024-08-17 23:09:21 +0900

    fix: hover時のスタイル修正

--- src/ui/components/category-chip.tsx
+++ src/ui/components/category-chip.tsx
@@ -8,7 +8,7 @@ const CategoryChip: React.FC<Props> = ({ category: { id, label } }) => {
   return (
     <Link
       href={`/blog/category/${id}/1`}
-      className="text-white bg-gray-300 rounded text-sm px-3"
+      className="text-white bg-gray-300 rounded text-sm px-3 hover:bg-gray-400 duration-200"
     >
       {label}
     </Link>
