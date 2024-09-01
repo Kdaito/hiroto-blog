515a1e580f8ae68928e3c736746876eb799d348e
Kdaito <kdaito0620@gmail.com>
2024-09-01 17:31:23 +0900

    fix: フリーワード検索投稿一覧ページで実際の投稿を表示する

--- src/ui/pages/post-search-all/container.tsx
+++ src/ui/pages/post-search-all/container.tsx
@@ -1,3 +1,4 @@
+import { getBlogListBySearch } from "@/api/cms/blog";
 import PostSearchAllPresenter from "@/ui/pages/post-search-all/presenter";
 import { searchParamSchema } from "@/validation/schema";
 import { SearchParamSchema } from "@/validation/schema/types";
@@ -9,7 +10,7 @@ const MAX_ITEMS = 8;
  *
  * Dynamic Rendring
  */
-export default function PostSearchAllContainer({
+export default async function PostSearchAllContainer({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -19,26 +20,32 @@ export default function PostSearchAllContainer({
   try {
     parsedParams = searchParamSchema.parse(searchParams);
   } catch {
+    // TODO エラーハンドリング
     return <div>パラメータが不正です</div>;
   }
 
   const { q: query, page: currentPage } = parsedParams;
 
-  const start = 1 + (currentPage - 1) * MAX_ITEMS;
-  const end = currentPage * MAX_ITEMS;
-  const resultCounts = `${start} ~ ${end}件目`;
+  const offset = (currentPage - 1) * MAX_ITEMS;
 
+  const blogList = await getBlogListBySearch({
+    offset,
+    limit: MAX_ITEMS,
+    keyword: query,
+  });
+
+  const end = currentPage * MAX_ITEMS;
+  const resultCounts = `${offset + 1} ~ ${
+    blogList.blogs.length < end ? blogList.blogs.length : end
+  }件目`;
   const basePath = "/blog/search?q=" + query;
   const pageTitle = query + "の検索結果";
 
-  // TODO fetch data
-  const totalItems = 100;
-
   return (
     <PostSearchAllPresenter
+      blogList={blogList}
       pageTitle={pageTitle}
       resultCounts={resultCounts}
-      totalItems={totalItems}
       currentPage={currentPage}
       maxItems={MAX_ITEMS}
       basePAth={basePath}
