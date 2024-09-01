d6b6f331641c048611c7c32babda911b44a9851a
Kdaito <kdaito0620@gmail.com>
2024-09-01 17:21:10 +0900

    fix: ページ全体のレイアウトの高さとフッターの位置を修正

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -7,12 +7,14 @@ export default function BlogListLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Header />
-      <div className="w-[80%] max-w-[1000px] mx-auto mt-10 mb-16">
-        {children}
+    <main className="min-h-[100vh] flex flex-col justify-between">
+      <div>
+        <Header />
+        <div className="w-[80%] max-w-[1000px] mx-auto mt-10 mb-16">
+          {children}
+        </div>
       </div>
       <Footer />
-    </>
+    </main>
   );
 }
