4cab13e5aeb7b97793a2652ad517d3c5a05914ad
Kdaito <kdaito0620@gmail.com>
2024-09-01 17:30:17 +0900

    fix: 投稿一覧ページで実際の投稿を表示する

--- src/ui/pages/post-all/container.tsx
+++ src/ui/pages/post-all/container.tsx
@@ -1,28 +1,31 @@
+import { getBlogList } from "@/api/cms/blog";
 import PostAllPresenter from "./presenter";
 
 const MAX_ITEMS = 8;
 const BASE_PATH = "/blog";
 const PAGE_TITLE = "All Posts";
 
-export default function PostAllContainer({
+export default async function PostAllContainer({
   params,
 }: {
   params: { id: string };
 }) {
   const currentPage = parseInt(params.id, 10);
 
-  const start = 1 + (currentPage - 1) * MAX_ITEMS;
-  const end = currentPage * MAX_ITEMS;
-  const resultCounts = `${start} ~ ${end}件目`;
+  const offset = (currentPage - 1) * MAX_ITEMS;
 
-  // TODO fetch data
-  const totalItems = 100;
+  const blogList = await getBlogList({ offset: offset, limit: MAX_ITEMS });
+
+  const end = currentPage * MAX_ITEMS;
+  const resultCounts = `${offset + 1} ~ ${
+    blogList.blogs.length < end ? blogList.blogs.length : end
+  }件目`;
 
   return (
     <PostAllPresenter
+      blogList={blogList}
       pageTitle={PAGE_TITLE}
       resultCounts={resultCounts}
-      totalItems={totalItems}
       currentPage={currentPage}
       maxItems={MAX_ITEMS}
       basePAth={BASE_PATH}
--- src/ui/pages/post-all/presenter.tsx
+++ src/ui/pages/post-all/presenter.tsx
@@ -1,42 +1,45 @@
+import { BlogList } from "@/type/blog";
 import List from "@/ui/components/list";
 import PageNation from "@/ui/components/pagination";
 import SideContent from "@/ui/components/side-content";
 
 type Props = {
+  blogList: BlogList;
   pageTitle: string;
   resultCounts: string;
-  totalItems: number;
   currentPage: number;
   maxItems: number;
   basePAth: string;
 };
 
 const PostAllPresenter: React.FC<Props> = ({
+  blogList,
   pageTitle,
   resultCounts,
-  totalItems,
   currentPage,
   maxItems,
   basePAth,
-}) => {
-  return (
-    <div className="flex gap-5 relative">
-      <div>
-        <List pageTitle={pageTitle} resultCounts={resultCounts} />
-        <div className="flex justify-center py-8">
-          <PageNation
-            total={totalItems}
-            current={currentPage}
-            pageSize={maxItems}
-            basePath={basePAth}
-          />
-        </div>
-      </div>
-      <div className="sticky top-0 w-[280px] h-[200px] pt-16">
-        <SideContent />
+}) => (
+  <div className="flex gap-5 relative">
+    <div>
+      <List
+        pageTitle={pageTitle}
+        blogs={blogList.blogs}
+        resultCounts={resultCounts}
+      />
+      <div className="flex justify-center py-8">
+        <PageNation
+          total={blogList.totalCount}
+          current={currentPage}
+          pageSize={maxItems}
+          basePath={basePAth}
+        />
       </div>
     </div>
-  );
-};
+    <div className="sticky top-0 w-[280px] h-[200px] pt-16">
+      <SideContent />
+    </div>
+  </div>
+);
 
 export default PostAllPresenter;
